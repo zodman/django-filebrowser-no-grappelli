a9fc95fbfdc892b467b14a9491be2af5ba73f524
Sukharev Maxim <maxim.sukharev@lazada.com>
2016-02-24 15:43:42 +0700

    remove grappelli from tests

--- tests/urls.py
+++ tests/urls.py
@@ -7,6 +7,5 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/filebrowser/', include(site.urls)),
-    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
