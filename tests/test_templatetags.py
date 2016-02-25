273ee70db957572d3a78cf8ca00ccba478d0340d
Sukharev Maxim <maxim.sukharev@lazada.com>
2016-02-25 12:07:08 +0700

    fix tests of get_file_extensions template tag

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -8,7 +8,7 @@ from filebrowser.templatetags.fb_tags import get_file_extensions
 class GetFileExtensionsTemplateTagTests(TestCase):
     def test_get_all(self):
         self.assertEqual(
-            sorted(get_file_extensions('')),
+            sorted(eval(get_file_extensions(''))),
             sorted([
                 '.pdf', '.doc', '.rtf', '.txt', '.xls', '.csv', '.docx', '.mov',
                 '.wmv', '.mpeg', '.mpg', '.avi', '.rm', '.jpg', '.jpeg', '.gif', '.png',
@@ -18,5 +18,5 @@ class GetFileExtensionsTemplateTagTests(TestCase):
     def test_get_filtered(self):
         self.assertEqual(
             get_file_extensions(QueryDict('type=image')),
-            ['.jpg', '.jpeg', '.gif', '.png', '.tif', '.tiff']
+            "['.jpg', '.jpeg', '.gif', '.png', '.tif', '.tiff']"
         )
